51d51cc194e8a69a693c23b89e2f320978955f67
saturnSolar <saturnsolar1093@gmail.com>
2025-05-19 21:43:30 +0800

    Added blur and directional transition

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-let slideIndex = 1;
+var slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n, direction) {
+  showSlides(slideIndex += n, direction);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function showSlides(n, direction) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
     let details = document.getElementsByClassName("details_container");
+    
     if (n > slides.length) {slideIndex = 1;}
     if (n < 1) {slideIndex = slides.length;}
-    for (i = 0; i <slides.length; i++) {
+    for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
         details[i].style.display = "none";
     }
+    let thumb = slides[slideIndex-1].getElementsByClassName("thumb");
+   if (direction == "next") {
+      thumb[0].style.animation = "fadeleft 1s forwards";
+    } else if (direction == "prev") {
+      thumb[0].style.animation = "faderight 1s forwards";
+    }
     slides[slideIndex-1].style.display = "block";
-    details[slideIndex-1].style.display = "flex";
+    details[slideIndex-1].style.display = "flex"
     setTimeout(showSlides, 2000);
 }
