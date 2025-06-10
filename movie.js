2ab0d909afa6a7efe4e950e198dda56b106311cb
saturnSolar <saturnsolar1093@gmail.com>
2025-06-11 07:04:24 +0800

    Added dynamic background.

--- movie.js
+++ movie.js
@@ -21,30 +21,36 @@ function displayMovie(data) {
     const movie = data[movieId];
     title.textContent = movie.name + " | Makmurflix";
     if (movie) {
-        container.innerHTML = `
-        <h1>${movie.name}</h1>
-        <p>${movie.year} | ${movie.duration}</p>
-        <hr>
-        <div class="container-row">
-            <img src="${movie.poster}" class="poster" alt="${movie.name}">
-            <div class="movie-details">
-                <div class="container-genre">
-                    ${movie.genres.map(genre => `<div>${genre}</div>`).join('')}
-                </div>
-                <p class="synopsis">${movie.desc}</p>
-                <hr>
-                <p><span style="font-weight: bold;">Writer</span> ${movie.writer}</p>
-                <hr>
-                <p><span style="font-weight: bold;">Producer</span> ${movie.producer}</p>
-                <hr>
-                <p><span style="font-weight: bold;">Cast</span> ${movie.cast}</p>
-                <hr>
-                <div class="btn" onclick="window.location.href='comparison.html'";>Find available cinema</div>
-            </div>
-        </div>
-        `
+      container.style.background = `linear-gradient(to bottom, rgba(0, 0, 0, .8), rgba(0, 0, 0, .1) 30%),
+        linear-gradient(to top, rgba(0, 0, 0, .8), rgba(0, 0, 0, .4)),
+        linear-gradient(to right, rgba(0, 0, 0, .8), rgba(0, 0, 0, .1) 30%),
+        linear-gradient(to left, rgba(0, 0, 0, .8), rgba(0, 0, 0, .1) 30%),url(${movie.thumb})`;
+      container.style.backgroundSize = "cover";
+
+      container.innerHTML = `
+      <h1>${movie.name}</h1>
+      <p>${movie.year} | ${movie.duration}</p>
+      <hr>
+      <div class="container-row">
+          <img src="${movie.poster}" class="poster" alt="${movie.name}">
+          <div class="movie-details">
+              <div class="container-genre">
+                  ${movie.genres.map(genre => `<div>${genre}</div>`).join('')}
+              </div>
+              <p class="synopsis">${movie.desc}</p>
+              <hr>
+              <p><span style="font-weight: bold;">Writer</span> ${movie.writer}</p>
+              <hr>
+              <p><span style="font-weight: bold;">Producer</span> ${movie.producer}</p>
+              <hr>
+              <p><span style="font-weight: bold;">Cast</span> ${movie.cast}</p>
+              <hr>
+              <div class="btn" onclick="window.location.href='comparison.html'";>Find available cinema</div>
+          </div>
+      </div>
+      `;
     } else {
-        container.innerHTML = `<h1>Movie not found</h1>`
+        container.innerHTML = `<h1>Movie not found</h1>`;
     }
 }
 
