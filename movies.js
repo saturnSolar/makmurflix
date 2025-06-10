c993362b3a26d2439c701462bc0dd1e28a73afd0
saturnSolar <saturnsolar1093@gmail.com>
2025-06-11 05:15:12 +0800

    Added sorting mechanism.

--- movies.js
+++ movies.js
@@ -43,6 +43,52 @@ for (let i = 0; i < filteroption.length; i++) {
   });
 }
 
-function filter(value) {
-  
+const sortbtn = document.getElementsByClassName("sort-btn")[0];
+const sortcont = document.getElementsByClassName("sort-cont")[0];
+
+sortbtn.addEventListener("click", () => sortcont.classList.toggle("hidden"));
+
+const sortoption = sortcont.getElementsByTagName("option");
+
+for (let i = 0; i < sortoption.length; i++) {
+  sortoption[i].addEventListener("click", function() {
+    var list, i, switching, b, shouldSwitch;
+  list = document.getElementById("movieGrid");
+  switching = true;
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    b = list.getElementsByClassName("movie-card");
+    // Loop through all list items:
+    for (i = 0; i < (b.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Check if the next item should
+      switch place with the current item: */
+      if (this.value == "title") {
+        if (b[i].getElementsByTagName("h3")[0].innerHTML.toLowerCase() > b[i + 1].getElementsByTagName("h3")[0].innerHTML.toLowerCase()) {
+          /* If next item is alphabetically lower than current item,
+          mark as a switch and break the loop: */
+          shouldSwitch = true;
+          break;
+        }
+      } else if (this.value == "rating") {
+        if (b[i].getElementsByClassName("rating")[0].innerHTML < b[i + 1].getElementsByClassName("rating")[0].innerHTML) {
+          /* If next item is alphabetically lower than current item,
+          mark as a switch and break the loop: */
+          shouldSwitch = true;
+          break;
+        }
+      } 
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark the switch as done: */
+      b[i].parentNode.insertBefore(b[i + 1], b[i]);
+      switching = true;
+    }
+  }
+  });
 }
