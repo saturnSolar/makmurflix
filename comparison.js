63c57fa651c43469cf9885142e01c91dbf8ecf5e
saturnSolar <saturnsolar1093@gmail.com>
2025-06-11 03:39:53 +0800

    Added a bunch of cool comparison feature.

--- comparison.js
+++ comparison.js
@@ -4,7 +4,7 @@ const ticketPricesDiv = document.getElementById('ticket-prices');
 const cinemaSelect = document.getElementsByClassName('cinema-selector');
 var cinema1;
 var cinema2;
-
+var movie;
 
 
 // const updateTicketPrices = () => {
@@ -42,6 +42,7 @@ fetch("cinemas.json")
     .then(data => {
         for (let x = 0; x < cinemaSelect.length; x++) {
             const dropdownMenu = cinemaSelect[x].getElementsByClassName('cinema-dropdown-menu')[0];
+            const select = cinemaSelect[x].getElementsByClassName('select')[0];
             for (let y = 0; y < data.length; y++) {
                 const cinemadata = data[y];
                 const option = document.createElement('option');
@@ -49,8 +50,12 @@ fetch("cinemas.json")
                 option.textContent = cinemadata.name;
                 option.addEventListener('click', function() {
                     cinemaSelect[x].querySelector('p').innerHTML = cinemadata.name;
-                    cinemaSelect[x].value = cinemadata.id;
-                    console.log(cinemaSelect[x].value);
+                    if (x == 0) {
+                        cinema1 = cinemadata.id;
+                    } else {
+                        cinema2 = cinemadata.id;
+                    }
+                    Compare();
                 });
                 dropdownMenu.appendChild(option);
             }
@@ -68,8 +73,9 @@ fetch("movies.json")
             option.textContent = moviedata.name;
             option.addEventListener('click', function() {
                 movieSelect.querySelector('label').innerHTML = moviedata.name;
-                movieSelect.value = moviedata.id;
-                console.log(movieSelect.value);
+                movie = moviedata.id;
+                console.log(movie);
+                Compare();
             });
             dropdownMenu.appendChild(option);
         }
@@ -103,3 +109,93 @@ document.addEventListener('click' , function(event) {
         }
     }
 })
+
+function Compare() {
+    console.log(movie);
+    console.log(cinema1);
+    console.log(cinema2);
+    const cinema1Select = document.getElementById('cinema1');
+    const cinema2Select = document.getElementById('cinema2');
+    console.log(cinema1Select);
+    if (movie && cinema1 && cinema2) {
+        fetch("data.json")
+            .then(res => res.json())
+            .then(data => {
+                data.forEach(cinema => {
+                    if (cinema.cinemaid == cinema1) {
+                        display(cinema1Select, cinema);
+                    }
+                    if (cinema.cinemaid == cinema2) {
+                        display(cinema2Select, cinema);
+                    }
+                })
+            })
+    } else {console.log("NOPE");}
+}
+
+function display(cinema, data) {
+    const itemcont = cinema.getElementsByClassName('item-cont');
+    const warningmessage =cinema.getElementsByClassName('warning-message')[0];
+    console.log(itemcont);
+    console.log("HI");
+    warningmessage.classList.add('hidden');
+    for (let x = 0; x < itemcont.length; x++) {
+        itemcont[x].classList.remove("hidden");
+    }
+
+
+    const datecontainer = itemcont[0].querySelector('article').querySelector('section');
+    const timeline = datecontainer.parentNode.getElementsByClassName('timeline-cont')[0].getElementsByClassName('dot-cont')[0];
+    timeline.innerHTML = '';
+    console.log(datecontainer);
+    let air = data.air;
+    for (let x = 0; x < air.length; x++) {
+        if (air[x].movieid == movie) {
+            datecontainer.innerHTML = '';
+            for (availability in air[x].availability) {
+                const day = document.createElement('div');
+                day.className = 'day-cont';
+                day.innerHTML = `
+                    <h5 class="day">${air[x].availability[availability].date.dayofweek}</h5>
+                    <h2 class="date">${air[x].availability[availability].date.day}</h2>
+                    <h5 class="month">${air[x].availability[availability].date.month}</h5>`
+                day.addEventListener('click', function() {
+                    const index = Array.from(datecontainer.children).indexOf(day);
+                    console.log(index);
+                    const timeavailable = air[x].availability[index].time;
+                    console.log(index);
+                    console.log(availability);
+                    console.log(timeavailable);
+                    timeline.innerHTML = '';
+                    for (let i = 0; i < timeavailable.length; i++) {
+                        const time = document.createElement('div');
+                        time.className = 'dot';
+                        time.style.left = ((timeavailable[i]-8)/16)*100 + '%';
+                        timeline.appendChild(time);
+                    }
+                })
+                datecontainer.appendChild(day);     
+            }
+            break;
+        }
+    }
+
+    const experiences = data.experiences;
+    console.log(experiences);
+    const experiencecont = itemcont[1].getElementsByClassName('experience-cont')[0];
+    experiencecont.innerHTML = '';
+    var actualExperiencesData;
+      fetch("experiences.json")
+        .then(res => res.json())
+        .then(data => {
+            actualExperiencesData = data;
+            console.log(actualExperiencesData);
+            for (x in experiences) {
+                const experience = document.createElement('div');
+                experience.className = 'exp';
+                experience.innerHTML = `
+                    <img src="${actualExperiencesData[experiences[x]].icon}">`;
+                experiencecont.appendChild(experience);
+            }
+        })
+}
