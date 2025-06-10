02f21d14bebd63143b3d4c0d50cfe6159b980948
saturnSolar <saturnsolar1093@gmail.com>
2025-06-11 04:35:59 +0800

    Added filtering for movies.

--- movies.js
+++ movies.js
@@ -11,10 +11,38 @@ fetch("movies.json")
       <div class="movie-info">
         <div class="rating">★ ${movie.rating}</div>
         <h3>${movie.name}</h3>
-        <p>${movie.year} • ${movie.duration}</p>
+        <div class="genre-cont">
+          ${movie.genres.map(genre => `<p>${genre}</p>`).join('')}
+        </div>
         <button class="details-btn" onclick='window.location.href="movie.html?id=${movie.id}"')">Details</button>
       </div>
     `;
     grid.appendChild(card);
   });
 })
+
+const filterbtn = document.getElementsByClassName("filter-btn")[0];
+const filtercont = document.getElementsByClassName("filter-cont")[0];
+
+filterbtn.addEventListener("click", () => filtercont.classList.toggle("hidden"));
+
+const filteroption = filtercont.getElementsByTagName("option");
+for (let i = 0; i < filteroption.length; i++) {
+  console.log(filteroption[i]);
+  filteroption[i].addEventListener("click", function() {
+    const cards = document.getElementsByClassName("movie-card");
+  console.log('h');
+  for (let i = 0; i < cards.length; i++) {
+    console.log(cards[i]);
+    if (cards[i].innerHTML.includes(this.value)) {
+      cards[i].style.display = "flex";
+    } else {
+      cards[i].style.display = "none";
+    }
+  }
+  });
+}
+
+function filter(value) {
+  
+}
