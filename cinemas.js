e8328223823238acbc8f767ad19ff5ba285c360e
saturnSolar <saturnsolar1093@gmail.com>
2025-06-11 10:04:36 +0800

    Fixed filter and sort functionality.

--- cinemas.js
+++ cinemas.js
@@ -21,7 +21,7 @@ fetch('cinemas.json')
           <div class="details">
             <span class="status-dot ${cinema.status === 'open' ? 'open' : 'closed'}"></span>
             <span>${cinema.name}</span><br>
-            <span><span style="color: #FFD700;">★</span> ${cinema.rating}</span><br>
+            <span class="rating"><span style="color: #FFD700;">★</span> ${cinema.rating}</span><br>
             <span>📍${cinema.city}</span>
           </div>
         </div>
@@ -38,29 +38,73 @@ fetch('cinemas.json')
     });
   });
 
-function showDetails(cinema) {
- /* cinema = JSON.parse(cinema);
-  document.getElementById('cinemaName').textContent = cinema.name;
-  document.getElementById('cinemaStatus').textContent = "Status: " + cinema.status;
-  document.getElementById('cinemaRating').textContent = "Rating: ⭐" + cinema.rating;
-  document.getElementById('cinemaContact').textContent = "Contact: 📞" + cinema.contact;
-  document.getElementById('cinemaDetailPopup').classList.remove('hidden');
-} 
+const filterbtn = document.getElementsByClassName("filter-btn")[0];
+const filtercont = document.getElementsByClassName("filter-cont")[0];
 
-  const cinemaDetailPopup = document.getElementById('cinemaDetailPopup');
-  const cinemaName = document.getElementById('cinemaName');
-  const cinemaStatus = document.getElementById('cinemaStatus');
-  const cinemaRating = document.getElementById('cinemaRating');
+filterbtn.addEventListener("click", () => filtercont.classList.toggle("hidden"));
 
-  cinemaName.textContent = cinema.name;
-  cinemaStatus.textContent = cinema.status;
-  cinemaRating.textContent = cinema.rating;
-
-  cinemaDetailPopup.classList.remove('hidden');
+const filteroption = filtercont.getElementsByTagName("option");
+for (let i = 0; i < filteroption.length; i++) {
+  console.log(filteroption[i]);
+  filteroption[i].addEventListener("click", function() {
+    const cards = document.getElementsByClassName("cinema-card");
+  for (let i = 0; i < cards.length; i++) {
+    console.log(cards[i]);
+    if (cards[i].innerHTML.includes(this.value)) {
+      cards[i].style.display = "flex";
+    } else {
+      cards[i].style.display = "none";
+    }
+  }
+  });
 }
 
- function closePopup() {
-  document.getElementById('cinemaDetailPopup').classList.add('hidden');
-} */
+const sortbtn = document.getElementsByClassName("sort-btn")[0];
+const sortcont = document.getElementsByClassName("sort-cont")[0];
 
-};
+sortbtn.addEventListener("click", () => sortcont.classList.toggle("hidden"));
+
+const sortoption = sortcont.getElementsByTagName("option");
+
+for (let i = 0; i < sortoption.length; i++) {
+  sortoption[i].addEventListener("click", function() {
+    var list, i, switching, b, shouldSwitch;
+  list = document.getElementById("cinemaList");
+  switching = true;
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    b = list.getElementsByClassName("cinema-card");
+    // Loop through all list items:
+    for (i = 0; i < (b.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Check if the next item should
+      switch place with the current item: */
+      if (this.value == "title") {
+        if (b[i].getElementsByTagName("span")[1].innerHTML.toLowerCase() > b[i + 1].getElementsByTagName("span")[1].innerHTML.toLowerCase()) {
+          /* If next item is alphabetically lower than current item,
+          mark as a switch and break the loop: */
+          shouldSwitch = true;
+          break;
+        }
+      } else if (this.value == "rating") {
+        if (b[i].getElementsByClassName("rating")[0].innerHTML < b[i + 1].getElementsByClassName("rating")[0].innerHTML) {
+          /* If next item is alphabetically lower than current item,
+          mark as a switch and break the loop: */
+          shouldSwitch = true;
+          break;
+        }
+      } 
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark the switch as done: */
+      b[i].parentNode.insertBefore(b[i + 1], b[i]);
+      switching = true;
+    }
+  }
+  });
+}
