883f75173f2e1e1811720d493c677712e0567cb8
saturnSolar <saturnsolar1093@gmail.com>
2025-06-11 09:52:13 +0800

    Added search functionality.

--- header.js
+++ header.js
@@ -1,4 +1,16 @@
 
+fetch("movies.json")
+  .then(res => res.json())
+  .then(data => {
+    const results = document.getElementsByClassName("results")[0];
+    data.forEach(movie => {
+      const card = document.createElement("a");
+      card.innerHTML = `${movie.name}`;
+      card.href = `movie.html?id=${movie.id}` 
+      results.appendChild(card);
+    });
+  })
+
 function showMenu() {
   var x = document.getElementsByClassName("navi")[0];
   if (x.className === "navi") {
@@ -20,14 +32,36 @@ function waitForElement(selector, callback) {
 
 
 waitForElement('.searchbar', function(element) {
+  const resultscont = document.getElementsByClassName("results")[0]
+  const children = resultscont.children;
   element.addEventListener("focus", function() {
     element.placeholder = "Search";
   })
-});
-
-waitForElement('.searchbar', function(element) {
+  element.addEventListener("input", function() {
+    
+    if (element.value == "") {
+      resultscont.style.display = "none";
+    } else {
+      resultscont.style.display = "block";}
+    
+    Array.from(children).forEach(item => {
+      if (item.innerHTML.toLowerCase().includes(element.value.toLowerCase())) {
+        item.style.display = "block";
+      } else {
+        item.style.display = "none";
+      }
+    })
+  })
   element.addEventListener("blur", function() {
     element.placeholder = "";
     element.value = "";
+    // resultscont.style.display = "none";
   })
+});
+
+document.addEventListener("click", function(event) {
+  const resultscont = document.getElementsByClassName("results")[0];
+  if (!resultscont.contains(event.target)) {
+    resultscont.style.display = "none";
+  }
 });
